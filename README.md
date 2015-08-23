609728fa157ab9428358055004d217e2e50fe055
enzo - Eduardo Garcia <josegarcia@appnovation.com>
2015-08-23 10:52:08 -0600

    Update logic to implement link to image styles and fix issue related with setting saved

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ Image Raw Field Formatter
 
 Image Raw Formatter for Drupal 8. This formatter return URLs absolute of original images or image style if is configured. The default formates form image doens't work for REST services because return HTML tags for images.
 
-### Install
+### Install using Drupal Console project
 ```bash
-$ cd path/to/drupal/8/modules
+$ cd path/to/drupal/8/modules/contrib
 $ git clone https://github.com/enzolutions/image_raw_formatter.git
-$ drush en -y image_raw_formatter # or enable this module via UI
+$ drupal module:install image_raw_formatter # or enable this module via UI
 ```
 
 ### Usage
@@ -15,6 +15,7 @@ $ drush en -y image_raw_formatter # or enable this module via UI
  * In your content type create a new Image field
  * Go to /admin/structure/types/manage/[Content-Type]/display
  * Change the format field to use Image Raw formatter
+ * Or select formatter in views fields images
 
 Resources
 ---------
--- src/Plugin/Field/FieldFormatter/ImageRawFormatter.php
+++ src/Plugin/Field/FieldFormatter/ImageRawFormatter.php
@@ -8,9 +8,17 @@
 namespace Drupal\image_raw_formatter\Plugin\Field\FieldFormatter;
 
 use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatterBase;
-use Drupal\image\Entity\ImageStyle;
-use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\UrlGeneratorInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Url;
+use Drupal\Core\Utility\LinkGeneratorInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Cache\Cache;
 use \InvalidArgumentException;
 
 /**
@@ -24,20 +32,102 @@ use \InvalidArgumentException;
  *   }
  * )
  */
-class ImageRawFormatter extends ImageFormatterBase
+class ImageRawFormatter extends ImageFormatterBase  implements ContainerFactoryPluginInterface
 {
+    /**
+     * The current user.
+     *
+     * @var \Drupal\Core\Session\AccountInterface
+     */
+    protected $currentUser;
 
-  /**
+    /**
+     * The link generator.
+     *
+     * @var \Drupal\Core\Utility\LinkGeneratorInterface
+     */
+    protected $linkGenerator;
+
+    /**
+     * The image style entity storage.
+     *
+     * @var \Drupal\Core\Entity\EntityStorageInterface
+     */
+    protected $imageStyleStorage;
+
+    /**
+     * Constructs an ImageFormatter object.
+     *
+     * @param string $plugin_id
+     *   The plugin_id for the formatter.
+     * @param mixed $plugin_definition
+     *   The plugin implementation definition.
+     * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+     *   The definition of the field to which the formatter is associated.
+     * @param array $settings
+     *   The formatter settings.
+     * @param string $label
+     *   The formatter label display setting.
+     * @param string $view_mode
+     *   The view mode.
+     * @param array $third_party_settings
+     *   Any third party settings settings.
+     * @param \Drupal\Core\Session\AccountInterface $current_user
+     *   The current user.
+     * @param \Drupal\Core\Utility\LinkGeneratorInterface $link_generator
+     *   The link generator service.
+     */
+    public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, AccountInterface $current_user, LinkGeneratorInterface $link_generator, EntityStorageInterface $image_style_storage) {
+        parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+        $this->currentUser = $current_user;
+        $this->linkGenerator = $link_generator;
+        $this->imageStyleStorage = $image_style_storage;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+        return new static(
+          $plugin_id,
+          $plugin_definition,
+          $configuration['field_definition'],
+          $configuration['settings'],
+          $configuration['label'],
+          $configuration['view_mode'],
+          $configuration['third_party_settings'],
+          $container->get('current_user'),
+          $container->get('link_generator'),
+          $container->get('entity.manager')->getStorage('image_style')
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function defaultSettings() {
+        return array(
+          'image_style' => '',
+          'image_link' => '',
+        ) + parent::defaultSettings();
+    }
+
+    /**
    * {@inheritdoc}
    */
   public function settingsForm(array $form, FormStateInterface $form_state) {
     $image_styles = image_style_options(FALSE);
+
     $element['image_style'] = array(
       '#title' => t('Image style'),
       '#type' => 'select',
       '#default_value' => $this->getSetting('image_style'),
       '#empty_option' => t('None (original image)'),
       '#options' => $image_styles,
+      '#description' => array(
+        '#markup' => $this->linkGenerator->generate($this->t('Configure Image Styles'), new Url('entity.image_style.collection')),
+        '#access' => $this->currentUser->hasPermission('administer image styles'),
+      ),
     );
 
     return $element;
@@ -85,7 +175,7 @@ class ImageRawFormatter extends ImageFormatterBase
 
         // Get image style URL
         if ($image_style) {
-          $image_uri = ImageStyle::load($image_style->getName())->buildUrl($image_uri);
+          $image_uri = $this->imageStyleStorage->load($image_style->getName())->buildUrl($image_uri);
         } else {
           // Get absolute path for original image
           $image_uri = $item->entity->url();
